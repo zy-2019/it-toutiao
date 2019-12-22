eba3a16b174e57589f8896ddd169ccfadb49f820
Your Name <you@example.com>
2019-12-22 22:11:13 +0800

    头条PC-素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,16 +1,27 @@
 <template>
     <el-card>
-        <bread-crumb>
+        <bread-crumb slot="header">
             <template slot="title">
                 素材管理
             </template>
         </bread-crumb>
+
+        <!-- 标签页 -->
+        <!-- 单词别再写错啦我是真的怕啦 -->
+        <el-tabs v-model="activeName">
+            <el-tab-pane label="全部图片" name="all">全部</el-tab-pane>
+            <el-tab-pane label="收藏图片" name="collect">收藏</el-tab-pane>
+        </el-tabs>
     </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all'
+    }
+  }
 }
 </script>
 
