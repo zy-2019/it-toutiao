5dac3b2512e013f41de02a1ccb7f00bb8fc87ceb
Your Name <you@example.com>
2019-12-26 14:17:16 +0800

    头条PC-发表文章-发布文章校验逻辑实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -9,41 +9,38 @@
         </bread-crumb>
 
         <!-- 页面结构搭建 -->
+        <el-form style="margin-left:15px" label-width="100px" ref="publishForm" :model="formData" :rules="publishRules">
 
-        <el-form style="margin-left:15px" label-width="100px">
-
-            <el-form-item label="标题">
-                <el-input placeholder="请输入标题" style="width:60%"></el-input>
+            <el-form-item label="标题" prop="title">
+                <el-input placeholder="请输入标题" style="width:60%" v-model="formData.title"></el-input>
             </el-form-item>
     <!-- --------------------------------------------------------------------- -->
-            <el-form-item label="内容">
+            <el-form-item label="内容" prop="content" >
 
-                <el-input type="textarea" :rows="4">
+                <el-input v-model="formData.content" type="textarea" :rows="4">
 
                 </el-input>
 
             </el-form-item>
     <!-- ---------------------------------------------------------------------- -->
-            <el-form-item label="封面">
-                <el-radio-group>
-                    <el-radio>单图</el-radio>
-                    <el-radio>三图</el-radio>
-                    <el-radio>无图</el-radio>
-                    <el-radio>自动</el-radio>
+            <el-form-item label="封面" prop="cover">
+                <el-radio-group v-model="formData.cover.type">
+                    <el-radio :label="1">单图</el-radio>
+                    <el-radio :label="3">三图</el-radio>
+                    <el-radio :label="0">无图</el-radio>
+                    <el-radio :label="-1">自动</el-radio>
                 </el-radio-group>
             </el-form-item>
     <!-- ------------------------------------------------------------------------- -->
-            <el-form-item label="频道">
-                <el-select>
-
-                    <!-- 根据elementUI id是存储值  name是显示值 -->
-
+            <el-form-item label="频道" prop="channel_id">
+                <el-select v-model="formData.channel_id">
+                    <!-- 根据elementUI对应的规则 id是存储值  name是显示值 -->
                     <el-option v-for='item in channels' :key="item.id" :value="item.id" :label="item.name"></el-option>
                 </el-select>
             </el-form-item>
     <!-- ------------------------------------------------------------------------- -->
             <el-form-item>
-                <el-button type="primary">发布文章</el-button>
+                <el-button type="primary" @click="publishArticles">发布文章</el-button>
                 <el-button>存入草稿</el-button>
             </el-form-item>
 
@@ -56,7 +53,31 @@
 export default {
   data () {
     return {
-      channels: [] // 定义接收频道数据
+      channels: [], // 定义接收频道数据
+
+      // 表单校验和校验规则绑定  在每一项里边需要v-model双向进行绑定
+      formData: {
+        title: '', // 文章标题
+        content: '', // 文章内容
+        // 文章封面类型 -1:自动，0-无图，1-1张，3-3张
+        cover: {
+          type: 0, // 默认先给个0
+
+          images: [] // 放制封面地址的数组
+        },
+        channel_id: null // 文章所属频道id  先给个null
+      },
+
+      //   自动校验规则  title  content  channel_id   是必填项  prop是要校验的字段名
+      publishRules: {
+        title: [{ required: true, message: '文章标题不能为空' }, {
+          min: 5,
+          man: 25,
+          message: '标题的长度在5到25个字符之间'
+        }],
+        content: [{ required: true, message: '文章内容不能为空' }],
+        channel_id: [{ required: true, message: '文章频道不能为空' }]
+      }
     }
   },
   methods: {
@@ -66,10 +87,21 @@ export default {
       }).then(res => {
         this.channels = res.data.channels
       })
+    },
+
+    // 手动校验表单方法
+    publishArticles () {
+      this.$refs.publishForm.validate(isOK => {
+        if (isOK) {
+          console.log('校验通过')
+        }
+      })
     }
   },
+
   created () {
-    this.getChannels()
+    this.getChannels() // 加载频道数据的方法
+    // this.publishArticles()
   }
 
 }
